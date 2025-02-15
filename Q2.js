b02f05a069de35f10b78fc2d577fc81cd90d70c5
Ashwin Menghar <ashwinmenghar123@gmail.com>
2025-02-15 11:55:05 +0530

    Q2 completed

--- Q2.js
+++ Q2.js
@@ -1,4 +1,14 @@
 import people from "./people.js";
 
 // Group the users based on the profession:
-people.reduce(() => {}, {});
+const getGroupOfUsers = () => {
+  return people.reduce((result, engineer) => {
+    if (!result[engineer.profession]) {
+      result[engineer.profession] = [];
+    }
+    result[engineer.profession].push(engineer.name);
+    return result;
+  }, {});
+};
+
+console.log(getGroupOfUsers());
